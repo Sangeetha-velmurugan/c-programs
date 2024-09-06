cfbc311431d903707e5b7f66d3b3b5ee98ce7527
Sangeetha V <sangeethamuthu2006@gmail.com>
2024-09-06 11:43:36 +0530

    Add files via upload

---
+++ ARITHMET.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdio.h>
+void main()
+{
+int a,b,add,mul,div,sub;
+clrscr();
+printf("\n simple calculation");
+printf("\n enter number1:");
+scanf("%d",&a);
+printf(" enter number2:");
+scanf("%d",&b);
+add=a+b;
+sub=a-b;
+mul=a*b;
+div=a/b;
+printf("\n  the sum is %d",add);
+printf("\n the sub is %d",sub);
+printf(" \n the mul is %d",mul);
+printf("\n the div is %d",div);
+getch();
+}
+
