cc78446639c930efc73631f934eae4e01b69802f
Sangeetha V <sangeethamuthu2006@gmail.com>
2024-09-06 12:56:27 +0530

    Add files via upload

---
+++ SQUARE.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,sq;
+clrscr();
+printf("enter the value of a:");
+scanf("%d",&a);
+sq=a*a;
+printf(" the square of the given value is %d",sq);
+getch();
+return 0;
+}
