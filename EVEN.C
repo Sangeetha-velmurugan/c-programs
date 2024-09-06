c22cc476fbc63148b7869ff03e8fac25d0f1ed97
Sangeetha V <sangeethamuthu2006@gmail.com>
2024-09-06 12:36:01 +0530

    Update EVEN.C

--- EVEN.C
+++ EVEN.C
@@ -1,16 +1,13 @@
 #include<stdio.h>
-#include<conio.h>
 int main()
 {
 int num1;
-clrscr();
 printf("\n enter a number 1:");
 scanf("%d",&num1);
 if(num1%2==0)
 printf("%d is even");
 else
 printf("%d is odd");
-getch();
 return 0;
 }
 
