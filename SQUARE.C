646a69f2124920cb4c5e6a0e02693f4fae55c3f2
Sangeetha V <sangeethamuthu2006@gmail.com>
2024-09-06 12:56:56 +0530

    Update SQUARE.C

--- SQUARE.C
+++ SQUARE.C
@@ -1,13 +1,10 @@
 #include<stdio.h>
-#include<conio.h>
 int main()
 {
 int a,sq;
-clrscr();
 printf("enter the value of a:");
 scanf("%d",&a);
 sq=a*a;
 printf(" the square of the given value is %d",sq);
-getch();
 return 0;
 }
