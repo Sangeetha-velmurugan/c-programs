facb3aa89cf03318861de0e6332560cf91be10ce
Sangeetha V <sangeethamuthu2006@gmail.com>
2024-09-06 12:27:08 +0530

    Update DISCOUNT.C

--- DISCOUNT.C
+++ DISCOUNT.C
@@ -1,18 +1,15 @@
 #inlcude<stdio.h>
-#include<conio.h>
 void main()
 {
-int amt,tot_amt1,tot_amt2;
-clrscr();
+float amt,tot_amt1,tot_amt2;
 printf("enter the bill amount:");
-scanf("%d",&amt);
-if(amt>=1000 && amt>=5000 )
-printf("you have got 25% discount");
+scanf("%f",&amt);
+if(amt>=1000 && amt<=5000 )
+printf("you have got 25 percent discount");
 tot_amt1=(amt*25)/100;
-printf("with discount total amount is %d",tot_amt1);
-else if(amt>=5000 && amt>=10000)
-printf("you have got 50% discount");
+printf("with discount total amount is %f",tot_amt1);
+else if(amt>=5000 && amt<=10000)
+printf("you have got 50 percent discount");
 tot_amt2=(amt*50)/100;
-printf("with discount total amount is %d",tot_amt2);
-getch();
+printf("with discount total amount is %f",tot_amt2);
 }
