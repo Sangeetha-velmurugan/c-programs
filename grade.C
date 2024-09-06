7b91cdc52ce9c29331dc7f380bf0110ba39a0be0
Sangeetha V <sangeethamuthu2006@gmail.com>
2024-09-06 12:41:20 +0530

    Update grade.C

--- grade.C
+++ grade.C
@@ -1,14 +1,11 @@
 #include<stdio.h>
-#include<conio.h>
 void main()
 {
-int mark;
-clrscr();
+int mark;
 printf("enter the mark1:");
 scanf("%d",&mark);
 if(mark>=80 && mark<=100)
 printf("you have got A grade");
 else if(mark>=40 && mark<80)
-printf("you have got B grade");
-getch();
+printf("you have got B grade");
 }
