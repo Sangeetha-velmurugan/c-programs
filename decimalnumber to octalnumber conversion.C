9e4668b4e144736af738afe439590bd2e096fa47
Sangeetha V <sangeethamuthu2006@gmail.com>
2024-09-08 10:27:10 +0530

    Add files via upload

---
+++ decimalnumber to octalnumber conversion.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int decimalnumber,remainder,sum=0,placevalue=1;
+printf("enter the decimal number: \n");
+scanf("%d",&decimalnumber);
+while(decimalnumber!=0)
+{
+reminder=decimalnumber%8;
+decimalnumber=decimalnumber/8;
+sum=sum+remainder*placevalue;
+placevalue=placevalue*10;
+}
+printf("the octal number is %d",sum);
+}
