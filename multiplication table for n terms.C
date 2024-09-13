1fe369e2bd6637f13f4ec27b60af1417c0b9eff0
Sangeetha V <sangeethamuthu2006@gmail.com>
2024-09-13 11:43:12 +0530

    Add files via upload

---
+++ multiplication table for n terms.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,j;
+printf("enter the value of n:");
+for(int i=1;i<=n;i++)
+{
+for(j=1;j<10;;j
+++)
+{
+printf("%d\t",(i*j));
+}
+printf("\n");
+}
