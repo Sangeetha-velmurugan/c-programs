6c903d7a66bbb097a770bbacc4c5d9f3d3385576
Sangeetha V <sangeethamuthu2006@gmail.com>
2024-09-16 19:49:54 +0530

    Add files via upload

---
+++ STAR  PATTERN USING DO WHILE.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i=1,n;
+clrscr();
+printf("enter the number of rows:");
+scanf("%d",&n);
+do
+{
+printf("*");
+}
+printf("\n");
+while(i<=n);
+{
+int j=1;
+while(j<=i);
+}
+getch();
+}
