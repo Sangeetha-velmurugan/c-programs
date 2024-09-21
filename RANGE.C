4623cab75ed965130c92924705a09666b69fc785
Sangeetha V <sangeethamuthu2006@gmail.com>
2024-09-21 13:59:46 +0530

    Add files via upload

---
+++ RANGE.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num1,num2,remainder,divided,divisor;
+printf("enter the number:");
+scanf("%d",&num1);
+printf("enter the number:");
+scanf("%d",&num2);
+if(num1>num2)
+divided=num1;
+else
+divisor=num2;
+while(num2!=0)
+{
+remainder=divided%divisor;
+if(remainder==0)
+{
+dividede=divisor;
+break;
+}
+divided=divisor;
+divisor=remainder;
+}
+printf("gcd of the number %d and %d is ",num1,num2,divided);
+getch();
+}
+
