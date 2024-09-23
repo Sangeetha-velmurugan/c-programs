9f77a8651c78cf422a76cbee4e1d6086a06b5f92
Sangeetha V <sangeethamuthu2006@gmail.com>
2024-09-23 17:50:49 +0530

    Update difference.c

--- difference.c
+++ difference.c
@@ -2,12 +2,11 @@
 void main()
 {
 int range,n,min=9,max=0,diff,i;
-
 printf("Enter a list of numbers : ");
 scanf("%d",&range);
 for(i=1;i<=range;i++)
 {
-printf("Enter number : ",i);
+printf("Enter number %d : ",i);
 scanf("%d",&n);
 if(n>max)
 {
