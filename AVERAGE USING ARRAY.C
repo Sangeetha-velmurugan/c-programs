9ef1e9f4a8a5f4af5ba9117d35847cad882fc502
Sangeetha V <sangeethamuthu2006@gmail.com>
2024-09-23 20:18:00 +0530

    Add files via upload

---
+++ AVERAGE USING ARRAY.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num[10],n,i,max=0,min=9;
+clrscr();
+printf("enter how many numbers:");
+scanf("%d",n);
+for(i=1;i<=n;i++)
+{
+printf("num %d:",i);
+scanf("%d",num[i]);
+if(num[i]>n)
+{
+max=num[i];
+}
+else
+{
+min=num[i];
+}
+printf("minimum number:",min);
+printf("maximum number:",max);
+getch();
+}
