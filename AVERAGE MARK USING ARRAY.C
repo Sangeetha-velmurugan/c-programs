969284bfe5a75445fc60121e6f305e51399931f3
Sangeetha V <sangeethamuthu2006@gmail.com>
2024-09-30 15:03:57 +0530

    Update AVERAGE MARK USING ARRAY.C

--- AVERAGE MARK USING ARRAY.C
+++ AVERAGE MARK USING ARRAY.C
@@ -1,9 +1,7 @@
 #include<stdio.h>
-#include<conio.h>
 void main()
 {
-int st_id,avg,total,subject,st_mark[n],n,i,j;
-clrscr();
+int st_id,avg,total,subject,st_mark[30],n,i,j;
 printf("enter the number of student:");
 scanf("%d",&n);
 printf("enter the number of subject:");
@@ -14,6 +12,13 @@ printf("enter the student id:");
 scanf("%d",&st_id);
 for(j=0;j<i;j++)
 {
-printf("\n enter mark %d:",j);
-scanf("%d",&mark[j])
+printf("\n enter mark %d:",j+1);
+scanf("%d",&st_mark[j]);
+total=total+st_mark[j];
+}
+printf("\n total mark:%d",total);
+printf("\n average :%f,avg);
+}
+}
+
 
