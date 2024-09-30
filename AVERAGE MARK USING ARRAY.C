435811f79f43b7d9f6dcce437d50cb8190db057e
Sangeetha V <sangeethamuthu2006@gmail.com>
2024-09-30 14:58:39 +0530

    Add files via upload

---
+++ AVERAGE MARK USING ARRAY.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int st_id,avg,total,subject,st_mark[n],n,i,j;
+clrscr();
+printf("enter the number of student:");
+scanf("%d",&n);
+printf("enter the number of subject:");
+scanf("%d",&subject);
+for(i=0;i<n;i++)
+{
+printf("enter the student id:");
+scanf("%d",&st_id);
+for(j=0;j<i;j++)
+{
+printf("\n enter mark %d:",j);
+scanf("%d",&mark[j])
+
