651b79e18dc9942168617744b5f4815015f3b29b
Sangeetha V <sangeethamuthu2006@gmail.com>
2024-10-07 15:34:44 +0530

    Update and rename COPY.C to COPY OF THE ELEMENT FROM ARRAY 1 TO ARRAY 2.C

--- COPY OF THE ELEMENT FROM ARRAY 1 TO ARRAY 2.C
+++ COPY OF THE ELEMENT FROM ARRAY 1 TO ARRAY 2.C
@@ -1,9 +1,7 @@
 #include<stdio.h>
-#include<conio.h>
 void main()
 {
 int i,n,num1[10],num2[10];
-clrscr();
 printf("enter the number of element:");
 scanf("%d",&n);
 for(i=0;i<n;i++)
@@ -17,5 +15,4 @@ for(i=0;i<n;i++)
 num2[i]=num1[i];
 printf("\t %d",num2[i]);
 }
-getch();
 }
