bcd605cf0ffcaeb2ac4e31ebc263b17dd22af0c9
Sangeetha V <sangeethamuthu2006@gmail.com>
2024-10-07 22:05:48 +0530

    Add files via upload

---
+++ BINARY.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int binary(int num)
+{
+int rem,i=1,sum=0;
+while(num!=0)
+{
+rem=num%2;
+sum+=rem*i;
+num=num/2;
+i=i*10;
+}
+return(sum);
+}
+void main()
+{
+int x;
+clrscr();
+printf("enter x:");
+scanf("%d",&x);
+printf("%d",binary(x));
+getch();
+}
