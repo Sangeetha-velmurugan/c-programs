83e54b6c5dff20002beec2cfcb88ee582ca32500
Sangeetha V <sangeethamuthu2006@gmail.com>
2024-10-19 08:11:47 +0530

    Update EVEN.C

--- EVEN.C
+++ EVEN.C
@@ -5,9 +5,9 @@ int num1;
 printf("\n enter a number 1:");
 scanf("%d",&num1);
 if(num1%2==0)
-printf("%d is even");
+printf("%d is even",num1);
 else
-printf("%d is odd");
+printf("%d is odd",num1);
 return 0;
 }
 
